12e3f0931aec397309360db8d48009dce2fe53e9
mcelam00 <47633255+mcelam00@users.noreply.github.com>
2019-06-15 21:18:31 +0200

    Programa utilizando comandos pintar en consola

---
+++ Pintar_consola/Pintar_consola.ino
@@ -0,0 +1,26 @@
+/*
+ * en el setup
+ * Serial.beguin 
+ * en el loop
+ * Serial.prrint que pinta lo que quieras (si yo quiero imprimir un texto entre comillas)
+ * Serial.println que lo imprime con retorno de carro
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+Serial.print("Prueba arduino");
+Serial.print("Prueba arduino");
+}
